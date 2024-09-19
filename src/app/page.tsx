9c7879984ded0ce96e56f11a98c9c1615d82a607
Nurlan <nurlanimamali@gmail.com>
2024-09-19 13:54:48 +0200

    small change to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Header from "@/app/sections/header";
+import Hero from "@/app/sections/hero";
+import Info from "@/app/sections/info";
+
 export default function Home() {
-  return <div className="flex justify-center items-center h-screen font-semibold text-6xl">SUNNY SIDE</div>;
+  return (
+    <div className="px-10">
+      <Header />
+      <Hero />
+      <Info />
+    </div>
+  );
 }
