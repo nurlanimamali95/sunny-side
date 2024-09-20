e4b4de6dca865cf7ed94386cc1ea9b8a471c3935
Nurlan <nurlanimamali@gmail.com>
2024-09-20 18:55:00 +0200

    logo changed and also variables for fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { overpass, savate, abrazScript } from "@/app/fonts";
 import "./globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-poppins",
-});
 
 export const metadata: Metadata = {
   title: "SunnySide",
@@ -19,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    <html
+      lang="en"
+      className={`${overpass.variable} ${savate.variable} ${abrazScript.variable}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
