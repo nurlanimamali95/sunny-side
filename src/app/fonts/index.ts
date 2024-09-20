98a0163b393ea9701e5fd4768b160f4b90684db6
Nurlan <nurlanimamali@gmail.com>
2024-09-20 18:52:11 +0200

    new fonts added as per style guide

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+import { Overpass } from "next/font/google";
+
+export const savate = localFont({
+  src: [
+    {
+      path: "./savate/savate-italique.otf",
+      weight: "400",
+      style: "italique",
+    },
+    {
+      path: "./savate/savate-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-savate",
+});
+
+export const abrazScript = localFont({
+  src: "./abraz-script/ABRAZ1.ttf",
+  display: "swap",
+  variable: "--font-abraz-script",
+});
+
+export const overpass = Overpass({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-overpass",
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      overpass: ["var(--font-overpass)"],
+      savate: ["var(--font-savate)"],
+      "abraz-script": ["var(--font-abraz-script)"],
+    },
+    colors: {
+      "primary-black": "#000000",
+      "primary-white": "#FFFFFF",
+      "white-yellow": "#FFFCC2",
+      "mid-yellow": "#FFF985",
+      "dark-yellow": "#FFF20A",
+    },
     extend: {
       colors: {
         background: "var(--background)",
