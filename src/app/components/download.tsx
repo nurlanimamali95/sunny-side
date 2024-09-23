51366d38f3665a49ca52b0ea86cb3f3aa8175e0b
Nurlan <nurlanimamali@gmail.com>
2024-09-23 22:29:53 +0200

    links are updated

--- src/app/components/download.tsx
+++ src/app/components/download.tsx
@@ -2,17 +2,17 @@ function DownloadButton() {
   return (
     <div>
       <button className="relative group overflow-hidden pl-6 h-14 flex space-x-6 items-center bg-mid-yellow">
-        <span className="relative uppercase text-base text-white">
+        <span className="relative uppercase text-base text-primary-black ">
           Download
         </span>
         <div
           aria-hidden="true"
-          className="w-14 bg-mid-yellow transition duration-300 -translate-y-7 group-hover:translate-y-7"
+          className="w-14 bg-mid-yellow transition duration-300 -translate-y-7 group-hover:bg-dark-yellow group-hover:translate-y-7"
         >
           <div className="h-14 flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 m-auto fill-white"
+              className="h-5 w-5 m-auto fill-primary-black"
               viewBox="0 0 20 20"
               fill="currentColor"
             >
@@ -26,7 +26,7 @@ function DownloadButton() {
           <div className="h-14 flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 m-auto fill-white"
+              className="h-5 w-5 m-auto fill-primary-black"
               viewBox="0 0 20 20"
               fill="currentColor"
             >
@@ -46,9 +46,9 @@ function DownloadButton() {
 export default function DownloadSection() {
   return (
     <div>
-      <div className="max-w-[20rem] bg-primary-white p-4 h-fit rounded-lg shadow-md">
+      <div className="max-w-[20rem] bg-primary-black text-white-yellow hover:text-dark-yellow p-4 h-fit rounded-lg shadow-md">
         <h3 className="text-xl font-bold mb-4">Download Our Menu</h3>
-        <p className="text-sm text-gray-600 mb-6">
+        <p className="text-sm mb-6">
           Want to keep a copy of our menu for later? You can download it in PDF
           format.
         </p>
--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,11 +1,29 @@
+import Link from "next/link";
+
 export default function Nav() {
   return (
     <div className="p-5 pr-0 hidden lg:block">
       <ul className="flex gap-[5rem] font-medium text-black">
-        <li>Menu</li>
-        <li>Locations</li>
-        <li>About Us</li>
-        <li>Contact</li>
+        <li>
+          <Link href="#menu" >
+            Menu
+          </Link>
+        </li>
+        <li>
+          <Link href="#locations" >
+            Locations
+          </Link>
+        </li>
+        <li>
+          <Link href="#aboutus" >
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link href="#contact" >
+            Contact
+          </Link>
+        </li>
       </ul>
     </div>
   );
