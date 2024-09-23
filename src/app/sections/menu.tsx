5fb4c2880a7a0e71f50d3fc339bbb57a887ac9b4
Nurlan <nurlanimamali@gmail.com>
2024-09-23 12:41:23 +0200

    menu items grid changed

--- src/app/sections/menu.tsx
+++ src/app/sections/menu.tsx
@@ -3,13 +3,19 @@ import DownloadSection from "@/app/components/download";
 
 export default function Menu() {
   return (
-    <div>
+    <div className="container mx-auto px-4">
       <h2 className="font-bold text-2xl my-[7rem] text-center">
         Explore Our Menu
       </h2>
-      <div className="">
-        <MenuAccordion />
-        <DownloadSection />
+
+      <div className="flex flex-col lg:flex-row justify-between gap-8">
+        <div className="w-full md:w-[70%]">
+          <MenuAccordion />
+        </div>
+
+        <div className="w-full md:w-[30%]">
+          <DownloadSection />
+        </div>
       </div>
     </div>
   );
